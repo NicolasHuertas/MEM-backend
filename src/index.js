369517eab449fdf7eaebfc6e8fb19acf3e5832c0
NicolasHuertas <98675931+NicolasHuertas@users.noreply.github.com>
2023-07-04 10:17:59 -0500

    fixed db uri

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const uri = "mongodb+srv://nicolashuertas:8bG4qjX0M8nw4hb3@cluster0.catgcnu.mongodb.net/";
+const uri = "mongodb+srv://nicolashuertas:8bG4qjX0M8nw4hb3@cluster0.catgcnu.mongodb.net/?retryWrites=true&w=majority";
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
